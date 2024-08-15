a1e179bf06b552bc6a6f5d88e100b33e08262812
Duong TRINH <khanhduong.dav@gmail.com>
2024-08-15 15:43:39 +0200

    replace defaultValue by selectedOption and update unit tests

--- src/_test_/App.test.tsx
+++ src/_test_/App.test.tsx
@@ -1,10 +1,12 @@
-import { render, screen, cleanup  } from "@testing-library/react";
+import { render, screen, cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
 import { SelectItem } from "../lib";
 import { ReactElement } from "react";
+import { Option } from "../lib/SelectItem";
 
-const options = [
+// Options for testing
+const options1 = [
   {
     label: "Female",
     value: "female",
@@ -17,91 +19,125 @@ const options = [
     label: "Other",
     value: "other",
   },
+  {
+    label: "Disabled option",
+    value: "test",
+    disabled: true,
+  },
 ];
+const options2: Option[] = []; //Empty options array
 
-
-function setup(jsx : ReactElement) {
+// Utility function to setup user event and render
+function setup(jsx: ReactElement) {
   return {
     user: userEvent.setup(),
-    ...render(jsx)
-  }
-
+    ...render(jsx),
+  };
 }
 
-describe("renders Select item", () => {
+describe("SelectItem Component", () => {
   const handleSelect = jest.fn();
   afterEach(cleanup);
-  it("should render with its label and its default value when Select item has the default value", () => {
+
+  it("renders with label and sets the first option as the default value", () => {
     render(
       <SelectItem
         id="genre"
         className="select-genre"
-        defaultValue={{
-          label: "Select an option",
-          value: "none",
-          disabled: true
-        }}
         label="Genre"
         handleSelect={handleSelect}
         name="genre"
-        options={options}
+        options={options1}
       />
     );
-
+    //check if the label is rendered correctly
     expect(screen.getByLabelText("Genre")).toBeTruthy();
-    expect(screen.getByRole("combobox", { name: "Genre" })).toBeTruthy();
-    //verify if default value is correct
-    expect(screen.getByRole("combobox")).toHaveValue("none");
-    
-    //verify if default value is selected
-    const defaultOption = screen.getByRole("option", {
-      name: "Select an option",
-    }) as HTMLOptionElement;
-    expect(defaultOption.selected).toBe(true);
-    expect(defaultOption.disabled).toBe(true);
-    expect(defaultOption.hidden).toBe(false);
 
-  });
-  it("should render with its first value in array options when Select item doesn't have the default value", () => {
-    render(
-      <SelectItem
-        id="genre"
-        className="select-genre"
-        // value=""
-        label="Genre"
-        handleSelect={handleSelect}
-        name="genre"
-        options={options}
-      />
-    );
+    //check if the select element is rendered and the default value is the first option
+    const select = screen.getByRole("combobox", { name: "Genre" });
+    expect(select).toBeInTheDocument();
+    expect(select).toHaveValue(options1[0].value);
 
-    expect(screen.getByRole("combobox")).toHaveValue(options[0].value);
+    //verify that the first option is selected by default
     const firstOption = screen.getByRole("option", {
-      name: options[0].label,
+      name: options1[0].label,
     }) as HTMLOptionElement;
     expect(firstOption.selected).toBe(true);
 
+    // verify that the second option is not selected by default
     const secondOption = screen.getByRole("option", {
-      name: options[1].label,
+      name: options1[1].label,
     }) as HTMLOptionElement;
     expect(secondOption.selected).toBe(false);
+  });
+  it("renders with the selectedOption prop if provided", () => {
+    render(
+      <SelectItem
+        id="genre"
+        className="select-genre"
+        label="Genre"
+        selectedOption="test"
+        handleSelect={handleSelect}
+        name="genre"
+        options={options1}
+      />
+    );
 
+    // Check if the select element is rendered with the correct selected option
+    const select = screen.getByRole("combobox");
+    expect(select).toHaveValue("test");
+
+    // Verify that the disabled state of selectedOption is setted correctly
+    const selectedOption = screen.getByRole("option", {
+      name: "Disabled option",
+    }) as HTMLOptionElement;
+    expect(selectedOption.disabled).toBe(true);
   });
 
-  it("should select correct value on change", async() => {
-    const {user} = setup(<SelectItem
-      id="genre"
-      className="select-genre"
-      label="Genre"
-      handleSelect={handleSelect}
-      name="genre"
-      options={options}
-    />);
+  it("should select correct value on change", async () => {
+    const { user } = setup(
+      <SelectItem
+        id="genre"
+        className="select-genre"
+        label="Genre"
+        handleSelect={handleSelect}
+        name="genre"
+        options={options1}
+      />
+    );
+    //Simulate selecting an option
     await user.selectOptions(screen.getByRole("combobox"), "other");
     expect(screen.getByRole("combobox")).toHaveValue("other");
 
-    const otherOption = screen.getByRole("option", { name: "Other" }) as HTMLOptionElement;
-    expect(otherOption.selected).toBe(true);
-  })
+    //Verify that the selected option is updated
+    const select = screen.getByRole("combobox");
+    expect(select).toHaveValue("other");
 
+    //Verify that the selected option has selected attribute
+    const otherOption = screen.getByRole("option", {
+      name: "Other",
+    }) as HTMLOptionElement;
+    expect(otherOption.selected).toBe(true);
+
+    //Ensure that the handleSelect has been called
+    expect(handleSelect).toHaveBeenCalledTimes(1);
+  });
+  it("renders a default option with a message when no options are available", () => {
+    render(
+      <SelectItem
+        id="genre"
+        className="select-genre"
+        label="Genre"
+        handleSelect={handleSelect}
+        name="genre"
+        options={options2}
+      />
+    );
+    // Check if the default message is rendered in the select element
+    const defaultOption = screen.getByRole("option", {
+      name: "No options available",
+    }) as HTMLOptionElement;
+    expect(defaultOption.disabled).toBe(true);
+    expect(defaultOption.selected).toBe(true);
+  });
 });
--- src/lib/SelectItem.tsx
+++ src/lib/SelectItem.tsx
@@ -1,24 +1,17 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
-interface DefaultValue {
+export interface Option {
   label: string;
   value: string;
   disabled?: boolean;
-  hidden?: boolean;
 }
 
-interface Option {
-  label: string;
-  value: string;
-}
-
-interface SelectItemProps {
+export interface SelectItemProps {
   label: string;
   id: string;
   className?: string;
-  defaultValue?: DefaultValue;
   name: string;
-  // value?: string;
+  selectedOption?: string;
   options: Option[];
   handleSelect: (event: React.ChangeEvent<HTMLSelectElement>) => void;
 }
@@ -27,29 +20,20 @@ const SelectItem: React.FC<SelectItemProps> = ({
   label,
   id,
   className,
-  defaultValue,
   name,
   options,
+  selectedOption,
   handleSelect,
 }) => {
-  const [dValue, setDValue] = useState<string>(
-    defaultValue?.value || options[0]?.value
-  );
-
-  useEffect(() => {
-    if (defaultValue) {
-      setDValue(defaultValue.value);
-    } else if (options.length > 0) {
-      setDValue(options[0].value);
-    }
-  }, []);
-
+  const [value, setValue] = useState(options.length > 0 ? options[0].value : "");
   const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setDValue(event.target.value);
-    console.log("selected option:", event.target.value);
-
+    setValue(event.target.value);
     handleSelect(event);
   };
+  // if(options.length === 0) {
+  //   console.error(`Error: There are no option to select ${label}`)
+  //   return
+  // }
 
   return (
     <>
@@ -58,26 +42,27 @@ const SelectItem: React.FC<SelectItemProps> = ({
         name={name}
         id={id}
         className={className}
-        value={dValue}
+        value={selectedOption || value}
         onChange={handleChange}
       >
-        {defaultValue && (
-          <option
-            value={defaultValue.value}
-            disabled={defaultValue.disabled}
-            hidden={defaultValue.hidden}
-          >
-            {defaultValue.label}
+         {options.length > 0 ? (
+          options.map((option) => (
+            <option
+              key={option.value}
+              value={option.value}
+              disabled={option.disabled}
+            >
+              {option.label}
+            </option>
+          ))
+        ) : (
+          // in case of no option
+          <option value="" disabled>
+            No options available
           </option>
         )}
-        {options.map((option) => (
-          <option key={option.value} value={option.value}>
-            {option.label}
-          </option>
-        ))}
       </select>
     </>
   );
 };
-
 export default SelectItem;
